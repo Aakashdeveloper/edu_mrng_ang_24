af39795ab349cdf8c67ed9c7dc73faf4480b6c31
aakashdeveloper <ahanda205@gmail.com>
2024-03-09 22:49:27 +0530

    homepage

--- fullstack/src/app/app.module.ts
+++ fullstack/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component'
 import { HeaderComponent } from './Header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './home/Search/search.component';
+import { QuickSearchComponent } from './home/QuickSearch/quickSearch.component';
 
 @NgModule({
 
     // All the component and pipe
     declarations:[
         AppComponent,
-        HeaderComponent
+        HeaderComponent,
+        FooterComponent,
+        HomeComponent,
+        SearchComponent,
+        QuickSearchComponent
     ],
 
     // All modules
---
+++ fullstack/src/app/home/QuickSearch/quickSearch.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector:'app-quickSearch',
+    templateUrl:'./quickSearch.component.html',
+    styleUrls:['./quickSearch.component.css']
+})
+
+export class QuickSearchComponent{}
